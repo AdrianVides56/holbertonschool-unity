8ddb075959a3ca5a62cef1d1b8123a12e854ac5d
AdrianVides56 <adrianvj18@gmail.com>
2021-09-29 16:21:45 -0500

    Added win canvas

--- 0x06-unity-assets_ui/Assets/Scripts/WinTrigger.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/WinTrigger.cs
@@ -3,18 +3,22 @@ using UnityEngine.UI;
 
 public class WinTrigger : MonoBehaviour
 {
-    public GameObject player, winTrigger;
-    public Text timer;
+    public GameObject player, winTrigger, winCanvas;
+    public Text timer, winText;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<Collider>().tag == "Player")
         {
+            Cursor.visible = true;
             Text textPosition = timer.GetComponent<Text>();
-            textPosition.transform.position += new Vector3(0, -10f, 0);
-            timer.color = Color.green;
-            timer.fontSize = 100;
             player.GetComponent<Timer>().enabled = false;
             winTrigger.GetComponent<Collider>().enabled = false;
+            winText.text = textPosition.text;
+            textPosition.enabled = false;
+            winCanvas.SetActive(true);
+            player.GetComponent<PauseMenu>().enabled = false;
+            Time.timeScale = 0f;
         }
     }
 }
