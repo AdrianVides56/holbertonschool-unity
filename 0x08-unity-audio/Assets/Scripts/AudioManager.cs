248b590b55a1519a18c510ff2ef7d00e715eca20
AdrianVides56 <adrianvj18@gmail.com>
2021-10-19 12:34:17 -0500

    Added sound when player clicks on a button in MainMenu scene

--- 0x08-unity-audio/Assets/Scripts/AudioManager.cs
+++ 0x08-unity-audio/Assets/Scripts/AudioManager.cs
@@ -4,8 +4,15 @@ using UnityEngine.Audio;
 public class AudioManager : MonoBehaviour
 {
     public AudioSource rollover;
+    public AudioSource click;
+
     public void ButtonRollover()
     {
         rollover.Play();
+    }
+
+    public void ButtonClick()
+    {
+        click.Play();
     }
 }
